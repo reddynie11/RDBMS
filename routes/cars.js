62a15d0ea2dae1b5b3c8f5d65375db3170dc0b3a
Narasimha Reddy <reddy.nie11@gmail.com>
2019-07-04 13:40:54 +0530

    integrating car model in cars route

--- routes/cars.js
+++ routes/cars.js
@@ -1,8 +1,28 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req,res)=>{
-    res.json('cars page');
+//import model
+const Seller = require('../models/seller');
+const Cars = require('../models/cars');
+
+//cars endpoint or routes
+router.post('/:id', (req,res)=>{
+    const car = new Cars();
+    const sellerid = req.params.id; // destructuring
+    car.model = req.body.formmodel;
+    car.year = req.body.formyear;
+    Seller.findById(sellerid, (err, data)=>{
+        car.seller = data;
+        car.save((err)=>{
+            if (err){throw err;}
+            else{res.json({'Status': 'Car deails saved'})}
+        });
+    });
+    
 });
+
+//router.get('/', (req,res)=>{
+//    res.json('cars page');
+//});
 
 module.exports = router;
